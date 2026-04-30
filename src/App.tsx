a5e5913dc928af704dcf6c822cb2d4f6333dd37b
Gyulo94 <qkrcjfrb112@gmail.com>
2026-04-30 09:33:56 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="cursor-pointer">버튼</Button>
+    </>
+  );
+}
+
+export default App;
